5c6ba9784e70d3d4345f9eeb790d0542d3d579ff
Chants77 <2426837835@qq.com>
2023-05-09 21:49:27 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ import {
 } from "react-router-dom";
 import default_router from "./routes/default_router";
 import App from "./App";
+
+axios.defaults.baseURL = 'http://localhost:8081'
+// axios.defaults.baseURL = 'http://svc-backend.default:8081'
+// axios.defaults.baseURL = 'http://116.204.69.134:30881'
+// axios.defaults.baseURL = 'http://localhost:8083'
+// axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+axios.defaults.headers["Content-Type"] = 'application/json';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
