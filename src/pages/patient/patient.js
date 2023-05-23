9309792abeeafa544eaff7fd9ddb442d040aa9c3
Will Frog <froggywch@gmail.com>
2023-05-24 00:19:06 +0800

    Finish myBill.js full functionality

--- src/pages/patient/patient.js
+++ src/pages/patient/patient.js
@@ -2,7 +2,7 @@ import {Layout} from "antd";
 import MyHeader from "../../components/header/header";
 import PatientSidebar from "./patientSidebar";
 import SelectDepartment from "./steps/selectDepartment";
-import React from "react";
+import React, {useState} from "react";
 import MakeAppointment from "./makeAppointment";
 import {Outlet} from "react-router-dom";
 import PatientHeader from "../../components/header/patientHeader";
@@ -10,13 +10,21 @@ const { Header, Footer, Sider, Content } = Layout;
 
 
 function Patient(){
+
+    // 当前患者的id，作为一个react state
+    // TODO 和其他组员沟通，应用里的患者ID存放在什么位置（localStorage还是？）
+    const [patientId, setPatientId] = useState(15);
+
+    // TODO 患者ID存放位置沟通完毕后，写一个useEffect hook来获取患者ID
+
+
     return (
         <div>
             <Layout style={{'height': '100%'}}>
                 <PatientHeader />
                 <Layout style={{'height': '600px'}}>
                     <Content>
-                        <Outlet />
+                        <Outlet context={[patientId, setPatientId]}/>
                     </Content>
                 </Layout>
                 <Footer>
